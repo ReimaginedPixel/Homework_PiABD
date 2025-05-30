775233a95cf8773b40561767a1d428809b90a6ef
ReimaginedPixel <pixelgameyt7@gmail.com>
2025-05-30 08:38:01 +0200

    aliasy

--- XAMPP/XAMPP mysql/Zadanie/3/rand.sql
+++ XAMPP/XAMPP mysql/Zadanie/3/rand.sql
@@ -5,3 +5,11 @@ select * from pracownicy;
 
 update pracownicy set id_stanowiska = rand();
 update pracownicy set id_stanowiska = rand()*(4)+1;
+select * from pracownicy;
+
+select pracownicy.imie, pracownicy.nazwisko, pracownicy.pesel, stanowiska.nazwa, stanowiska.pensja from pracownicy join stanowiska 
+on pracownicy.id_stanowiska = stanowiska.id_stanowiska;
+
+SELECT p.imie, p.nazwisko, p.pesel, s.nazwa, s.pensja
+FROM pracownicy AS p
+JOIN stanowiska AS s ON p.id_stanowiska = s.id_stanowiska;
